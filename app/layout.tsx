8cd49445ad4da91b97503279cd6b79b5daed0333
developer4u <usmanmukhtar246@gmail.com>
2026-08-24 06:42:42 +0500

    GTAG

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
-
+const GTM_ID = "GTM-MDQ8RLGH";
 export const metadata: Metadata = {
   title: {
     default: "Tax Calculator Pakistan | Navigate Business",
@@ -62,19 +62,19 @@ export default function RootLayout({
     >
     <head>
       <Script id="gtm-script" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-XXXXXXX');
-        `}
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
       </Script>
   </head>
       <body className="min-h-full flex flex-col font-sans max-w-screen overflow-x-hidden">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MDQ8RLGH"
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
